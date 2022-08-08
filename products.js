de430f09953f07cb387c0e4240ceddc4eb06f426
yLucxs7 <110850165+yLucxs7@users.noreply.github.com>
2022-08-08 15:23:04 -0300

    commit inicial

---
+++ products.js
@@ -0,0 +1,11 @@
+function verProdutos() {
+  var products = document.getElementById('card1');
+  products.classList.toggle('show')
+
+}
+
+function verProdutosLista() {
+  var products = document.getElementById('produtos-trigger');
+  products.classList.toggle('active')
+
+}
